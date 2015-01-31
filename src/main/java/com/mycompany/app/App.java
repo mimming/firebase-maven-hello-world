73c949eb8755bcb05adca0a271a84f19e3210dde
Jenny Tong <jen@firebase.com>
2015-01-31 12:38:08 -0800

    because deamon threads

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,34 @@
 package com.mycompany.app;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws InterruptedException {
         Firebase myFirebaseRef = new Firebase("https://dinosaurs.firebaseio.com/");
-        myFirebaseRef.child("dinosaurs").setValue("are cool");
+
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue()); 
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+
+        });
+
+        myFirebaseRef.child("dinosaurs").setValue("are neat");
+
+        
+        // Give it 5 seconds to execute, since they're running on deamon threads
+        Thread.sleep(5 * 1000);
     }
 }
